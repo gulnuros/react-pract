ac9c657d0e688a16df73fb8c1c87f21325ba9c52
gulnuros <93098410+gulnurkaztai@users.noreply.github.com>
2025-10-06 21:01:59 -0400

    app router

---
+++ react-next/src/app/contact/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <main>
+        <h1>Contact page</h1>
+      </main>
+    </>
+  );
+}
---
+++ react-next/src/app/mountain/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>A page about mountains</h1>
+    </main>
+  );
+}
