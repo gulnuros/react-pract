829f22a72e666b832390ca764db38985d2029909
gulnuros <93098410+gulnurkaztai@users.noreply.github.com>
2025-10-08 12:22:51 -0400

    create a server action

--- react-next/src/app/contact/page.js
+++ react-next/src/app/contact/page.js
@@ -1,8 +1,51 @@
 export default function Page() {
+  async function submitForm(formData) {
+    "use server";
+    const formFields = {
+      email: formData.get("email"),
+      message: formData.get("message"),
+    };
+    console.log("formFields", formFields);
+    console.log("todo: send these form field values to a backend");
+    return formFields;
+  }
   return (
     <>
-      <main>
-        <h1>Contact page</h1>
+      <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md text-gray-700">
+        <h1 className="text-2xl font-bold text-center mb-6">Contact page</h1>
+        <form action={submitForm} className="space-y-4">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium ">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="message" lassName="block text-sm font-medium ">
+              Message
+            </label>
+            <input
+              id="message"
+              type="message"
+              name="message"
+              rows="4"
+              className="border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-500 rounded-md p-3"
+          >
+            Send message
+          </button>
+        </form>
       </main>
     </>
   );
